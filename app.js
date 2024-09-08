e7ea9f487a21f2edc1875dae5d23d12d21f6b75b
SscottK <gr8whtninjassk@gmail.com>
2024-09-08 15:45:46 -0700

    All Exercises completed

--- app.js
+++ app.js
@@ -95,3 +95,100 @@ const userProfile = {
 }
 console.log("Exercise 6 answer: ", userProfile)
 
+
+/***************
+***EXERCISE 7***
+***************/
+//Practicing import and export. 
+
+
+/***************
+***EXERCISE 8***
+***************/
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+const description = (noun = "cat", adjective = "white") => {
+    return `The ${noun} is ${adjective}`
+}
+
+console.log("Exercise 8 answer: ", `no params: ${description()},`,` params: ${description("dog", "yellow")}`)
+
+
+/***************
+***EXERCISE 9***
+***************/
+
+// Convert the following `if...else` statement in to a ternary:
+
+// let pizza = 'tasty';
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+// Your code here
+const pizza = "tasty"
+
+let howItTasted = pizza ? "Yum" : "Yuck"
+
+console.log("Exercise 9 answer: ", howItTasted)
+
+
+/***************
+***EXERCISE 10**
+***************/
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+const LANG = localLangConfig || "en"
+// Log the result
+console.log("Exercise 10.1 answer: Language setting:", LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light'
+// Log the result
+console.log("Exercise 10.2 answer: User theme setting: ", USER_THEME);
+
+
+/***************
+***EXERCISE 11**
+***************/
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  let cat = adventurer.cat?.age; // Your code here
+  
+  console.log("Exercise 11 answer: ", cat);
+  
